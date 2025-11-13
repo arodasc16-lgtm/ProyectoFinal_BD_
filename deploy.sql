cd794a6dc946626037aae3081380640cd328c916
arodasc16-lgtm <arodasc16@miumg.edu.gt>
2025-11-12 21:53:29 -0600

    Create deploy.sql

---
+++ deploy.sql
@@ -0,0 +1,12 @@
+:r .\scripts\00_reset_y_esquemas.sql
+:r .\scripts\01_ddl_base.sql
+:r .\scripts\02_indices_y_sequences.sql
+:r .\scripts\03_vistas_schemabinding.sql
+:r .\scripts\04_pivot_y_window.sql
+:r .\scripts\05_optimizacion_planes.sql
+:r .\scripts\06_rls_alumnos.sql
+:r .\scripts\07_roles_permisos.sql
+:r .\scripts\08_auditoria_accesos.sql
+:r .\scripts\09_sesiones_alumno_sps.sql
+:r .\scripts\10_backup_full_diff.sql
+PRINT 'ProyectoFinal_BD desplegado correctamente.';
